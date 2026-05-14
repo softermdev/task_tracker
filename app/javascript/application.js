8e4761d600069805b9e309a5aa2b1eb26496363b
SofterM <66023096@up.ac.th>
2026-05-14 11:13:00 +0700

    update ui and toast noti

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,18 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+// Toast Notifications Auto-remove
+document.addEventListener('turbo:load', () => {
+  const toasts = document.querySelectorAll('.toast-notification');
+  
+  toasts.forEach((toast) => {
+    // Auto remove after animation completes (4.4 seconds)
+    setTimeout(() => {
+      if (toast.parentElement) {
+        toast.remove();
+      }
+    }, 4400);
+  });
+});
+
