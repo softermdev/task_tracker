a3872a27459cc97d87e3a4439a29983abb790f84
SofterM <66023096@up.ac.th>
2026-05-14 15:34:39 +0700

    displayname

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def user_display_name(user)
+    user.name.presence || user.email.split('@').first.capitalize
+  end
+
+  def user_initial(user)
+    (user.name.presence || user.email).first.upcase
+  end
 end
